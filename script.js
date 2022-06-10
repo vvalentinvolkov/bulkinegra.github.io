2afc4511c16b30cd03f92c825f80e9d02bf102d4
Valentin Volkov <v.valentinvolkov@gmail.com>
2022-06-10 15:26:40 +0300

    js

--- script.js
+++ script.js
@@ -2,15 +2,10 @@ let session_start = 0
 $( document ).ready(function() {
     session_start = new Date().getTime()
 });
-let tg = Telegram.WebApp
-tg.ready();
-tg.expand(); //расширяем на все окно
-tg.MainButton.text = "Задать вопрос"; //изменяем текст кнопки
-tg.MainButton.textColor = "#ffffff"; //изменяем цвет текста кнопки
-tg.MainButton.color = "#e88542"; //изменяем цвет бэкграунда кнопки
-
-Telegram.WebApp.onEvent('mainButtonClicked', function(){
-    let session_end = new Date().getTime()
-    tg.sendData(JSON.stringify({'session_time': session_end-session_start}));
-});
+Telegram.WebApp.ready();
+    Telegram.WebApp.MainButton.setText('Get contact').show().onClick(function () {
+        let session_end = new Date().getTime()
+        Telegram.WebApp.sendData(JSON.stringify({'session_time': session_end-session_start}));
+        Telegram.WebApp.close();
+    });
 // Usual js
